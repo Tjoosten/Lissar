8443195c9e9f1a379080e80d726d2f52c815eba3
Tim Joosten <Tjoosten3@gmail.com>
2017-11-12 20:58:32 +0100

    Implement buttons

--- resources/views/account-settings/information.blade.php
+++ resources/views/account-settings/information.blade.php
@@ -6,6 +6,18 @@
     <div class="panel-body">
         <form method="POST" action="" class="form-horizontal">
             {{ csrf_field() }} {{-- CSRF field protection --}}
+
+            <div class="form-group">
+                <div class="col-md-offset-3 col-md-9">
+                    <button type="submit" class="btn btn-success btn-sm">
+                        <i class="fa fa-check"></i> Wijzigen
+                    </button>
+
+                    <button type="reset" class="btn btn-link btn-sm">
+                        <i class="fa fa-undo"></i> Annuleren
+                    </button>
+                </div>
+            </div>
         </form>
     </div>
 </div>
