9b0f7d2e26f6214e3a5473ba726dc2abfdddca59
Tim Joosten <Tjoosten3@gmail.com>
2017-11-16 16:57:04 +0100

    Implement empty priorities controller

---
+++ app/Http/Controllers/PrioritiesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PrioritiesController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index(): View
+    {
+
+    }
+
+    public function create(): View
+    {
+
+    }
+
+    public function store(): RedirectResponse
+    {
+
+    }
+
+    public function edit(): View
+    {
+
+    }
+
+    public function update(): RedirectResponse
+    {
+
+    }
+
+    public function destroy(): RedirectResponse
+    {
+
+    }
+}
