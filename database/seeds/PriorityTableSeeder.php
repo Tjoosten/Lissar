cdf051a429f7efd56be009e8cf4ca1a4cf9dee7d
Tim Joosten <Tjoosten3@gmail.com>
2017-11-21 08:44:02 +0100

    Complete priority seeder

--- database/seeds/PriorityTableSeeder.php
+++ database/seeds/PriorityTableSeeder.php
@@ -20,16 +20,20 @@ class PriorityTableSeeder extends Seeder
         //! Insert data into the table. Factories used. 
         //! Because they are also implemented in the testing infterface.
 
-        factory(App\Priority::class)->create(
+        factory(App\Priority::class)->create( // Lage prioriteit
             ['name' => trans('seeders.priority-name-1'), 'description' => trans('seeders.priority-description-1'), 'color_code' => '#000']
         );
 
-        factory(App\Priority::class)->create(
+        factory(App\Priority::class)->create( // Gemiddelde prioriteit
             ['name' => trans('seeders.priority-name-2'), 'description' => trans('seeders.priority-description-2'), 'color_code' => '#000']
         );
 
+        factory(App\Priority::class)->create( // Hoge prioriteit
+            ['name' => trans('seeders.priority-name-3'), 'description' => trans('seeders.priority-description-3'), 'color_code' => '#000']
+        );
 
-        // Hoog
-        // Kritiek
+        factory(App\Priority::class)->create( // Kritieke prioriteit
+            ['name' => trans('seeders.priority-name-4'), 'description' => trans('seeders.priority-description-4'), 'color_code' => '#000']
+        );
     }
 }
---
+++ resources/lang/en/seeders.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Seeder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match with the standard 
+    | database seeds. Feel free to modify them to your needs.
+    |
+    */
+
+    // Helpdesk priority 
+    'priority-name-1' => 'Low',
+    'priority-name-2' => 'Medium',
+    'priority-name-3' => 'High',
+    'priority-name-4' => 'Critical',
+
+    'priority-description-1' => 'No direct attention required for these tickets',
+    'priority-description-2' => 'Attention required but no quick support or fix.',
+    'priority-description-3' => 'Tickets that are needed quick support or fix.',
+    'priority-description-4' => 'Issues or support that is needed ASAP.',
+
+];
